96726a05fe0f29219ea32d45d81b27bcf8608bb3
Aterushkov <Sanek.s88@gmail.com>
2020-07-07 14:11:55 +0500

    Second pages done

--- local/templates/empty/components/bitrix/news.detail/documents/template.php
+++ local/templates/empty/components/bitrix/news.detail/documents/template.php
@@ -17,9 +17,9 @@ $file = $arResult['PROPERTIES'];
 <? 
 
 				//echo '<pre>'. print_r($arResult, true) .'</pre>';
-				   echo '<pre>'. print_r($file, true) .'</pre>';  
+				  //  echo '<pre>'. print_r($file, true) .'</pre>';  
   ?>
-	<!-- <?=CFile::GetPath($arResult["PROPERTIES"]["PHOTO_THREE"]["VALUE"])?> -->
+	<!--  -->
 	<div class="row mx-6 py-4  mx-icon">
 		<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 			<?if($arResult['PROPERTIES']['NEED_H2']['VALUE_XML_ID'] == YES) :?>
@@ -28,26 +28,28 @@ $file = $arResult['PROPERTIES'];
 				<?endif;?>
 			<?endif;?>
 		</div>
-		<div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 mx-icon-p flex-wrap d-flex">
+		<div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 mx-icon-p flex-wrap d-flex px-0">
 				<?foreach ($file as $key => $val):?>
 					<?if($val['PROPERTY_TYPE'] == S):?>
 						<? $text ='<span class="span-icon">'.$val["VALUE"].'</span>'?>
 					<?endif;?>
 					<?if($val['PROPERTY_TYPE'] == F):?>
-							<?if($val['VALUE']):?>
-								<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex pb-3">
-									<div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 ">
-										<svg width="22" height="31" viewBox="0 0 22 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-											<rect x="1" y="1" width="20" height="23" rx="2" stroke="#F65E64" stroke-width="2"/>
-											<path d="M6 7.5H13" stroke="#F65E64" stroke-width="2"/>
-											<path d="M6 12.5H16" stroke="#F65E64" stroke-width="2"/>
-											<path d="M6 17.5H16" stroke="#F65E64" stroke-width="2"/>
-											<path d="M10 30H20" stroke="#F65E64" stroke-width="2"/>
-										</svg>
-									
+							<?if($val['VALUE']):?>	
+								<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex pb-3 px-0">
+									<div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 ">
+										<a href="<?=CFile::GetPath($val['VALUE'])?>">
+											<svg width="22" height="31" viewBox="0 0 22 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<rect x="1" y="1" width="20" height="23" rx="2" stroke="#F65E64" stroke-width="2"/>
+												<path d="M6 7.5H13" stroke="#F65E64" stroke-width="2"/>
+												<path d="M6 12.5H16" stroke="#F65E64" stroke-width="2"/>
+												<path d="M6 17.5H16" stroke="#F65E64" stroke-width="2"/>
+												<path d="M10 30H20" stroke="#F65E64" stroke-width="2"/>
+											</svg>
+										</a>
 									</div>
-									<div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 ">
-										<?=$text?>
+									<div class="col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11 ">
+									<a href="<?=CFile::GetPath($val['VALUE'])?>"><?=$text?></a>
+										
 									</div>
 								</div>
 							<?endif?>
